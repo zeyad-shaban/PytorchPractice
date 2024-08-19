7dfbe92c98f7b84743c1edb61787ddbf2faddcbc
zeyad-shaban <zeyadshapan2004@gmail.com>
2024-08-19 15:17:22 +0300

    init

--- main.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-points = torch.tensor([[4, 1], [5, 3], [2, 1]])
-points_t = points.t().contiguous()
-
-print(points_t.stride())
-
-print(points.is_contiguous())
-print(points_t.is_contiguous())
